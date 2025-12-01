64a9560a3cab903d082d1daebaa14dfa07c03de3
Ramirisa <iramirez@macalester.edu>
2025-12-01 00:10:48 -0600

    Proposal and Prototytpe commits

---
+++ Proposal.py
@@ -0,0 +1,11 @@
+#Title: Shooting game (Don't know official one yet)
+
+#Members: Isaiah R., Kuba F.
+
+#Description: Shooting game / Elimination of some sort
+#Want the user to be able to control movement of weapon/shooter while shooting at abject(s) appearing on screen.
+
+"""Inspiration: We totally changed direction of our initial idea, but we wanted to continue on the path of Video games.
+For each of our solo projects we created our own games about the sports we play. This time we wanted to form a game that
+reminds us of the older easier games you could download or play at an arcade like Fruit ninja or Galaga. Galaga
+was probably the main inspiration"""
